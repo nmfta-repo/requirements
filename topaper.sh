e7fe9cd08dfc8a62d5991d8dae6a8a2080efe622
Ben Gardiner <ben.l.gardiner@gmail.com>
2024-11-27 18:20:28 -0500

    update strictdoc invocation to use strictdoc.toml project configuration

--- topaper.sh
+++ topaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p reqif
-strictdoc export --formats=rst --project-title "Heavy Duty Vehicle Cybersecurity Requirements (HD VCR)" --output-dir . *.sdoc
+strictdoc export . --formats=rst --output-dir .
 mv rst/*.rst paper/source/
 
 (cd paper && make latex && make latexpdf)
